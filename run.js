a817f7add6a4dbed8a56a5d4c8aa8cb312729c90
Michaël van der Veen <pjvanderveen@msn.com>
2025-03-30 15:32:01 +0200

    changed config variables

--- run.js
+++ run.js
@@ -3,6 +3,7 @@ const exec = util.promisify(require('child_process').exec);
 
 async function lsExample() {
     try {
+        console.log(process.env.ACTUAL_SERVER_URL)
         const { stdout, stderr } = await exec('ls');
         console.log('stdout:', stdout);
         console.log('stderr:', stderr);
@@ -10,4 +11,6 @@ async function lsExample() {
         console.error(e); // should contain code (exit code) and signal (that caused the termination).
     }
 }
+
+
 lsExample()
