d907f76e45c683791aef8043755685bc5ba7bac8
Michaël van der Veen <pjvanderveen@msn.com>
2025-03-30 15:52:12 +0200

    Test getting env variables

--- run.js
+++ run.js
@@ -1,9 +1,13 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const fs = require('fs')
 
 async function lsExample() {
     try {
-        console.log(process.env.ACTUAL_SERVER_URL)
+        const options = fs.readFileAsync('/data/options.json')
+        console.log('options', options)
+        console.log('ENV', process.env.ACTUAL_SERVER_URL)
+
         const { stdout, stderr } = await exec('ls');
         console.log('stdout:', stdout);
         console.log('stderr:', stderr);
