19e2e20ea5b5ad2c9dbbba9b2c5ed5cd7867ad7a
Michaël van der Veen <pjvanderveen@msn.com>
2025-03-30 16:12:57 +0200

    Test getting env variables

--- run.js
+++ run.js
@@ -4,13 +4,14 @@ const fs = require('fs')
 
 async function lsExample() {
     try {
-        const options = fs.readFileSync('/data/options.json')
-        console.log('options', options)
+        // const options = fs.readFileSync('/data/options.json')
+        // console.log('options', options)
 
         console.log('ENV', process.env.ACTUAL_SERVER_URL)
 
 
-        const { stdout, stderr } = await exec('node apply-interest.js');
+        // const { stdout, stderr } = await exec('node apply-interest.js');
+        const { stdout, stderr } = await exec('ls /data/');
         console.log('stdout:', stdout);
         console.log('stderr:', stderr);
     } catch (e) {
